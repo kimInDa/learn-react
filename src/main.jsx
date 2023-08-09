9c39d33def0e21650f7b96af5cca6af3d530b651
kimInDa <pinktail2327@naver.com>
2023-08-09 16:03:24 +0900

    [3일차] 수업 내용 - JSX와 프론트엔드 개발 도구 - React 함수 컴포넌트

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+
+// import { createRoot } from '../node_modules/react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App'
+
+
+
+createRoot(document.getElementById('root'))
+.render(
+  // App 컴포넌트를 렌더링할 수 있도록 JSX 구문을 추가합니다.
+  <StrictMode>
+    <App />{/*<App></App>*/}
+  </StrictMode>
+  );
+  
+  // createRoot
+  // create + Root (React DOM Root)
+  // ReactDOMRoot 객체를 생성하는 함수
+  // ReactDOMRoot 객체는 render() 메서드를 사용해
+  // React 요소를 실제 DOM 요소에 렌더링 한다.
+  
+  
+  // JSX가 하는 일은 React 요소 생성 → 마크업 생성
+  // JSX → 컴파일러(변환기) → React 요소 → ReactDOM → 실제 DOM 요소 생성 → 구조 작성
+  
