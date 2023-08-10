3f71306c38203475026da353d2b6dee6ca13655d
kimInDa <pinktail2327@naver.com>
2023-08-10 15:40:56 +0900

    [4일차] 조건부 렌더링 vs. 조건부 표시

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,27 +1,15 @@
-import HeaderBar from './HeaderBar';
 import FooterBar from "./FooterBar";
+import HeaderBar from "./HeaderBar";
 
 function RootLayout(props) {
   return (
     <>
-      <HeaderBar key="header-bar" />, 
-      <main key="main">{props.children}</main>, 
-      <FooterBar key="footer-bar" />
+      {/* <div className="overlay"></div> */}
+      <HeaderBar />
+      <main>{props.children}</main>
+      <FooterBar />
     </>
-  )
-
-  
-  // JSX
-  // return은 값을 내보내므로 배열을 내보낼 수 있다.
-  // children = [] 배열이다.
-  // 대신 배열 내에서 고유한 key를 부여해줘야 한다.
-  // 이로 인해 쓸 데 없는 div로 감싸지 않아도 여러 형제를 내보낼 수 있다.
-  // return [
-  //   <HeaderBar key="header-bar" />, 
-  //   <main key="main">{props.children}</main>, 
-  //   <FooterBar key="footer-bar" />
-  // ];
+  );
 }
 
-
-export default RootLayout
+export default RootLayout;
