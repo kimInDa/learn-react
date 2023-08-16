0d3b5c268a7d909bcd7c560b4f1f6d0a9e87d018
kimInDa <pinktail2327@naver.com>
2023-08-16 10:37:20 +0900

    [7일차] 숫자 유형의 데이터 상태 관리 - count - step - currentYear

--- src/layout/HeaderBar.jsx
+++ src/layout/HeaderBar.jsx
@@ -1,12 +1,14 @@
-import styles from '@/styles/HeaderBar.module.css';
-import Heading from "@/components/Heading";
+import Heading from '@/components/Heading';
 
 function HeaderBar() {
+  // 리액트 학습 -> 사용 (필수 JavaScript 언어 구문 독해 및 활용 능력)
+  // var mood = '기운차게 시작;
+  // 템플릿 리터럴 구문 `오늘도 ${mood}하자!` -> string
   return (
-    <header className={styles.container}>
+    <header className='p-5 bg-indigo-950 text-indigo-50'>
       <Heading />
     </header>
-  )
+  );
 }
 
 export default HeaderBar;
