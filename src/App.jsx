e454ddb7800fbe7a7c5e0326fbd7be4f37e470fc
kimInDa <pinktail2327@naver.com>
2023-08-18 15:02:11 +0900

    [9일차] 내비게이션 활성화 설정 + 기본 스타일링

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, NavLink } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
 
-const Home = () => <div>Home</div>;
-const Products = () => <div>Products</div>;
-const Contact = () => <div>Contact</div>;
+const Nav = () => (
+  <nav>
+    <ul className='flex gap-4 p-5 font-extralight bg-slate-50/50 shadow-lg'>
+      <li>
+        <NavLink
+          to='/'
+          className={({ isActive }) =>
+            isActive ? 'font-semibold text-rose-600' : ''
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to='/products'
+          className={({ isActive }) =>
+            isActive ? 'font-semibold text-rose-600' : ''
+          }
+        >
+          Products
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to='/contact'
+          className={({ isActive }) =>
+            isActive ? 'font-semibold text-rose-600' : ''
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+
+const Home = () => (
+  <div>
+    <Nav />
+    <h1 className='text-black text-emerald-500'>Home</h1>
+  </div>
+);
+const Products = () => (
+  <div>
+    <Nav />
+    <h1 className='text-black text-emerald-500'>Products</h1>
+  </div>
+);
+const Contact = () => (
+  <div>
+    <Nav />
+    <h1 className='text-black text-emerald-500'>Contact</h1>
+  </div>
+);
 
 const router = createBrowserRouter([
   // Router Object
