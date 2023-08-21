3c6b0b58082f65ff6f167b352768ff6fe00ab908
kimInDa <pinktail2327@naver.com>
2023-08-21 15:11:06 +0900

    [10일차] useProductList 훅

--- src/routes.jsx
+++ src/routes.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="products" element={<Products />} />
-      <Route path="product/edit/:productName" element={<ProductEdit />} />
+      <Route path="product/edit/:productId" element={<ProductEdit />} />
       <Route path="contact" element={<Contact />} />
     </Route>
   )
