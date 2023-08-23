5a32a68ea27f4914034160c1c8091097ed5552c6
kimInDa <pinktail2327@naver.com>
2023-08-23 15:04:51 +0900

    [12일차] 컨텍스트 분리 관리 학습 준비

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -21,7 +21,7 @@ function RootLayout({ displaySideMenu = false }) {
                 <Link to="/learn/03">속성(props) 드릴링 이슈</Link>
               </li>
               <li>
-                <Link to="/learn/04">
+                <Link to="/learn/04/01">
                   상태 공유 Context{' '}
                   <abbr
                     title="Application Programming Interface"
@@ -30,6 +30,9 @@ function RootLayout({ displaySideMenu = false }) {
                     API
                   </abbr>
                 </Link>
+              </li>
+              <li>
+                <Link to="/learn/04/02">Context 분리 관리</Link>
               </li>
             </ul>
           </nav>
