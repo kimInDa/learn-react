c943a05ed680c8052310b36473a855e0cdd4f979
kimInDa <pinktail2327@naver.com>
2023-08-24 13:09:40 +0900

    [13일차] Web Animation API 명령형으로 제어
    - useRef() 기억(memo)
    - play()
    - pause()
    - stop()

--- src/learn/6-ref-referencing-dom.jsx
+++ src/learn/6-ref-referencing-dom.jsx
@@ -69,7 +69,7 @@ function Circle() {
   }, []);
 
   const handlePlayAnimation = () => {
-    if(!animationRef.current) {
+    if (!animationRef.current) {
       settingAnimation();
     } else {
       animationRef.current.play();
@@ -77,8 +77,21 @@ function Circle() {
   };
 
   const handlePauseAnimation = () => {
-    animationRef.current.pause();
+    if (animationRef.current) {
+      animationRef.current.pause();
+    }
   };
+
+  // Video, Audio
+  // play(), pause()
+  // stop: pause() && currentTime = 0
+  // const handleStopAnimation = () => {
+  //   if (animationRef.current) {
+  //     // stop() 사용자 구현
+  //     animationRef.current.pause();
+  //     animationRef.current.currentTime = 0;
+  //   }
+  // }
 
   return (
     <>
